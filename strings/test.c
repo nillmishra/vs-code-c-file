67fbd0c0877ca88bac290e7584b5bfd7cfe6b688
Nillotpal Mishra <nillmishra09@gmail.com>
2023-08-04 19:50:34 +0530

    test case reverse of string

---
+++ strings/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[20];
+    printf("Enter a string: ");
+    scanf("%[^\n]s", str);
+    printf("Your input is: %s\n", str);
+
+    int size = 0;
+    int k = 0;
+    while (str[k] != '\0') {
+        size++;
+        k++;
+    }
+    printf("The size is %d\n", size);
+
+    // Reverse the string
+    for (int i = 0; i < size / 2; i++) {
+        char temp = str[i];
+        str[i] = str[size - 1 - i];
+        str[size - 1 - i] = temp;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}

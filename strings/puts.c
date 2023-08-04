1179d58b4d24b8edb52efcd68148833e65578f9b
Nillotpal Mishra <nillmishra09@gmail.com>
2023-08-04 19:53:59 +0530

    puts

---
+++ strings/puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main(int argc, char const *argv[])
+{
+    char str[] ="College Wahllah is Best";
+
+    //printf("%s", str);
+    puts(str);
+    puts("Hello Baby");
+    return 0;
+}
